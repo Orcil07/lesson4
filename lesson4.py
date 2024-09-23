384b736a2d2636454cfb044e237642f49a174905
Orcil07 <orcil9675@gmail.com>
2024-09-23 22:19:08 +0300

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,12 @@
+my_string = input ("Нангияла я иду!")
+print ("Нангияла я иду!" .upper())
+my_string = input ("Нангияла я иду!")
+print ("Нангияла я иду!" .lower())
+my_string = input ("Нангила я иду!")
+print ("Нангияла я иду!" .replace(" ", " "))
+my_string = "Нангияла я иду!"
+print (my_string[0])
+my_string = "Нангияла я иду!"
+print (my_string [-1])
+
+
